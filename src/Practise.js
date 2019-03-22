980192c07f19b8623a8218aaf263a5ea6835c543
Athiya Sultana <athiya@olivetech.net>
2019-03-22 10:35:12 +0530

    practised click event

--- src/Practise.js
+++ src/Practise.js
@@ -3,29 +3,64 @@ import Book from "./Book";
 
 class Practise extends Component {
   state = {
+    showUserInfo: false,
     data: [
       {
         name: "Athiya Sultana",
         age: 22,
         email: "athiya@mailinator.com",
-        id: 1
+        id: 1,
+        ishidden: false
       },
-      { name: "Mounica", age: 23, email: "mounica@mailinator.com", id: 2 },
-      { name: "Swetha", age: 24, email: "swetha@mailinator.com", id: 3 }
+      {
+        name: "Mounica",
+        age: 23,
+        email: "mounica@mailinator.com",
+        id: 2,
+        ishidden: false
+      },
+      {
+        name: "Swetha",
+        age: 24,
+        email: "swetha@mailinator.com",
+        id: 3,
+        ishidden: false
+      }
     ]
   };
+
+  setInfo = function(user) {
+    console.log(user);
+    this.setState(function(prevState) {
+      return (user.ishidden = !user.ishidden);
+    });
+  };
+  // setInfo = (e, user, x) => {
+  //   console.log("user", e);
+  //   this.setState(function(prevState) {
+  //     return (user.ishidden = !user.ishidden);
+  //   });
+  // };
   render() {
     const { data } = this.state;
-    console.log("users", data);
     return (
       <div>
         {data.map(user => (
-          <Book
-            name={user.name}
-            key={user.id}
-            email={user.email}
-            age={user.age}
-          />
+          // <Book
+          //   name={user.name}
+          //   key={user.id}
+          //   email={user.email}
+          //   age={user.age}
+          // />
+          <div key={user.id}>
+            <h1 onClick={this.setInfo.bind(this, user)}>{user.name}</h1>
+            {user.ishidden ? (
+              <ul>
+                <li>{user.age}</li>
+                <li>{user.email}</li>
+              </ul>
+            ) : null}
+          </div>
         ))}
       </div>
     );
